6a395476196361f27509b3def6012f624e35f149
Leonid Morgenshtern <leonidmo@gmail.com>
2025-02-10 11:37:07 +0200

    Add Docker compose

--- pub_sub/csharp/sdk/checkout/Program.cs
+++ pub_sub/csharp/sdk/checkout/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using Dapr.Client;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Dapr.Client;
 
-for (int i = 1; i <= 10; i++) {
+await Task.Delay(TimeSpan.FromSeconds(1));
+for (int i = 1; i <= 10; i++)
+{
     var order = new Order(i);
     using var client = new DaprClientBuilder().Build();
 
